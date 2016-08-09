b0347304f3fa19d9b04d86568a5a5bf54efc6a03
飞飞 <zhaopengfeihappy@qq.com>
2016-08-10 07:57:08 +0800

    webview

--- app/src/main/java/cn/itcast/huayu/menu/activity/FisterActivity.java
+++ app/src/main/java/cn/itcast/huayu/menu/activity/FisterActivity.java
@@ -3,9 +3,12 @@ package cn.itcast.huayu.menu.activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.widget.TextView;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 import cn.itcast.huayu.menu.R;
 import cn.itcast.huayu.menu.model.menu.MenuDataVo;
@@ -16,8 +19,10 @@ import cn.itcast.huayu.menu.model.menu.MenuDataVo;
 @EActivity(R.layout.activity_first)
 public class FisterActivity extends BaseActivity {
 
+    @ViewById(R.id.webview)
+    WebView mWebView;
 
-    Handler mHandler = new Handler(){
+    Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
@@ -28,7 +33,10 @@ public class FisterActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
+
 
     @Override
     protected void onStart() {
@@ -39,8 +47,12 @@ public class FisterActivity extends BaseActivity {
     protected void onResume() {
         super.onResume();
         MenuDataVo mData = (MenuDataVo) getIntent().getSerializableExtra("mData");
-        TextView mTextView = (TextView) findViewById(R.id.tv_first);
-        mTextView.setText(mData.getTitle());
+        //打开网页
+        mWebView.loadUrl("http://www.runoob.com/");
+        // JavaScript使能(如果要加载的页面中有JS代码，则必须使能JS)
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        // 在WebView中打开链接（默认行为是使用浏览器，设置此项后都用WebView打开）
+        mWebView.setWebViewClient(new WebViewClient());
 
     }
 
@@ -49,4 +61,18 @@ public class FisterActivity extends BaseActivity {
         super.onDestroy();
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack())
+        {
+            // 返回键退回
+            mWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up
+        // to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
 }
